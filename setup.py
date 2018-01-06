0a8b1904e2985f8cd40aecec4ebffb617c94786a
Chris Knott <christopher.r.knott@gmail.com>
2018-01-06 17:53:11 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='Eel',
-    version='0.5.0',
+    version='0.6.1',
     author='Chris Knott',
+    author_email='chrisknott@hotmail.co.uk',
     packages=['eel'],
     package_data={
         'eel': ['eel.js'],
     },
     install_requires=['bottle', 'bottle-websocket'],
+    description='For little HTML GUI applications, with easy Python/JS interop',
     long_description=open('README.md', encoding='utf-8').readlines()[1],
     keywords=['gui', 'html', 'javascript', 'electron'],
 )
