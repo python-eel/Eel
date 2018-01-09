8bd909828f20a088a87f476b8d78948e921604ab
Chris Knott <christopher.r.knott@gmail.com>
2018-01-09 09:01:48 +0000

    bumped version, pushed to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Eel',
-    version='0.7.0',
+    version='0.8.0',
     author='Chris Knott',
     author_email='chrisknott@hotmail.co.uk',
     packages=['eel'],
