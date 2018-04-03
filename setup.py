9a65c9a325c5977371949eed42ed05fbfed13db2
Chris Knott <christopher.r.knott@gmail.com>
2018-04-03 12:36:53 +0100

    bump version to 0.9.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Eel',
-    version='0.9.1',
+    version='0.9.2',
     author='Chris Knott',
     author_email='chrisknott@hotmail.co.uk',
     packages=['eel'],
