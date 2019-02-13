25b7832efb19bc8548174145ec42cc53a63e03a7
Samuel Williams <samuelhwilliams@gmail.com>
2019-02-13 19:48:14 +0000

    Fix jinja template example
    The `templates` kwarg to `eel.start` takes a string rather than a
    boolean now, where the string is a filepath relative to eel's root
    directory.

--- examples/06 - jinja_templates/hello.py
+++ examples/06 - jinja_templates/hello.py
@@ -9,4 +9,4 @@ def say_hello_py(x):
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
 
-eel.start('templates/hello.html', size=(300, 200), templates=True)    # Start
+eel.start('templates/hello.html', size=(300, 200), templates='templates')    # Start
