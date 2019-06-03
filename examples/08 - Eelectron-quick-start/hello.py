d734071b2feb5972f20e214733108ebc7526a08f
Chris Knott <chrisknott@hotmail.co.uk>
2019-06-03 10:11:00 +0100

    New Electron example

---
+++ examples/08 - Eelectron-quick-start/hello.py
@@ -0,0 +1,21 @@
+from __future__ import print_function	# For Py2/3 compatibility
+import eel
+
+# Set web files folder
+eel.init('web')
+
+@eel.expose                         # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!')   # Call a Javascript function
+
+
+options = {
+	'mode': 'custom',
+	'args': ['node_modules/electron/dist/electron.exe', '.']
+}
+
+eel.start('hello.html', options=options)
+#eel.start('hello.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
