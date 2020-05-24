c91fb2b3e68b8f591c437b4899305c176f4d1127
Samuel Williams <samuelhwilliams@users.noreply.github.com>
2020-05-24 15:38:25 +0100

    Release for v0.12.4 (#303)
    * Update setup.py
    
    * Update CHANGELOG.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as read_me:
 
 setup(
     name='Eel',
-    version='0.12.3',
+    version='0.12.4',
     author='Chris Knott',
     author_email='chrisknott@hotmail.co.uk',
     url='https://github.com/samuelhwilliams/Eel',
