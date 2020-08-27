9deb9a2e2d319c3f9c052e647bccc2fb32457287
Mohaned Abd Elmonsef <6644557+mo0haned@users.noreply.github.com>
2020-08-27 21:25:46 +0200

    Update electron.py
    In windows, Electron is not in PATH by default. So, which will not work and it makes an error

--- eel/electron.py
+++ eel/electron.py
@@ -13,9 +13,7 @@ def run(path, options, start_urls):
 
 def find_path():
     if sys.platform in ['win32', 'win64']:
-        # It doesn't work well passing the .bat file to Popen, so we get the actual .exe
-        bat_path = wch.which('electron')
-        return os.path.join(bat_path, r'..\node_modules\electron\dist\electron.exe')
+        return r'..\node_modules\electron\dist\electron.exe'
     elif sys.platform in ['darwin', 'linux']:
         # This should work find...
         return wch.which('electron')
