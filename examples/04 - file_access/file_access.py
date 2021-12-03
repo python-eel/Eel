74eca0f12116fab8cc44524e37a31786c52a4f3f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-12-03 15:24:24 +0200

    refactor: update examples/04 - file_access/file_access.py

--- examples/04 - file_access/file_access.py
+++ examples/04 - file_access/file_access.py
@@ -1,12 +1,17 @@
-import eel, os, random
+import os, random
+
+
+import eel
 
 eel.init('web')
 
 @eel.expose
 def pick_file(folder):
     if os.path.isdir(folder):
-        return random.choice(os.listdir(folder))
-    else:
-        return 'Not valid folder'
+        files = os.listdir(folder)
+        
+        return random.choice(files)
+    
+    return 'Not a valid folder'
 
 eel.start('file_access.html', size=(320, 120))
