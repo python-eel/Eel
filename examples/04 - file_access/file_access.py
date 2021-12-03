e36b3af4a7e80e30e65e5b4b7506d49de7bdbcc5
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-12-03 15:26:42 +0200

    refactor: add "not"

--- examples/04 - file_access/file_access.py
+++ examples/04 - file_access/file_access.py
@@ -7,11 +7,11 @@ eel.init('web')
 
 @eel.expose
 def pick_file(folder):
-    if os.path.isdir(folder):
-        files = os.listdir(folder)
+    if not os.path.isdir(folder):
+        return 'Not a valid folder'
+
+    files = os.listdir(folder)
         
-        return random.choice(files)
-    
-    return 'Not a valid folder'
+    return random.choice(files)
 
 eel.start('file_access.html', size=(320, 120))
