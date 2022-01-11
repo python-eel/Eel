3f3e0ab257c52ea643a96794d9e202a99a344fec
malachov <malachovd@seznam.cz>
2022-01-11 22:24:28 +0100

    Update library versions + allow python 3.9 and 3.10

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: MIT License',
     ],
