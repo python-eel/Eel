ff5a7efa91c20470d8bb0d5ac0a46c29d1db2a02
Sentinent <thelin.json@gmail.com>
2022-03-11 23:59:01 -0800

    Update custom_app_routes example to not use beaker middleware, but include middleware usage in comments

--- examples/10 - custom_app_routes/custom_app.py
+++ examples/10 - custom_app_routes/custom_app.py
@@ -1,15 +1,17 @@
 import eel
 import bottle
-from beaker.middleware import SessionMiddleware
+# from beaker.middleware import SessionMiddleware
 
 app = bottle.Bottle()
 @app.route('/custom')
 def custom_route():
     return 'Hello, World!'
 
-# need to manually add eel routes if we are wrapping our Bottle instance with middleware
-eel.add_eel_routes(app)
-middleware = SessionMiddleware(app)
-
 eel.init('web')
-eel.start('index.html', app=middleware)
+
+# need to manually add eel routes if we are wrapping our Bottle instance with middleware
+# eel.add_eel_routes(app)
+# middleware = SessionMiddleware(app)
+# eel.start('index.html', app=middleware)
+
+eel.start('index.html', app=app)
