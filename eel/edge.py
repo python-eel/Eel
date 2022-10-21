68ab7a33fc1ee2115c4d1310967c1593ead804a9
Douglas Benn <fiery.coder@gmail.com>
2022-10-21 20:56:13 +0100

    Edge Build Crash Fix

--- eel/edge.py
+++ eel/edge.py
@@ -2,16 +2,16 @@ import platform
 import subprocess as sps
 import sys
 
-name = 'Edge'
+name = "Edge"
 
 
 def run(_path, options, start_urls):
-    cmd = 'start microsoft-edge:{}'.format(start_urls[0])
-    sps.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, stdin=sps.PIPE, shell=True)
+    cmd = "start microsoft-edge:{}".format(start_urls[0])
+    sps.Popen(cmd, stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE, shell=True)
 
 
 def find_path():
-    if platform.system() == 'Windows':
+    if platform.system() == "Windows":
         return True
 
     return False
