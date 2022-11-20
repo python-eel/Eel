4ebe94fdf870c5d67a21695586b50c8e93a94b19
Douglas Benn <fiery.coder@gmail.com>
2022-11-20 16:24:33 +0000

    Working Windows Tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,6 @@
 import contextlib
 import os
+import sys
 import subprocess
 import tempfile
 import time
@@ -8,15 +9,22 @@ from pathlib import Path
 import psutil
 
 # Path to the test data folder.
-TEST_DATA_DIR = Path(__file__).parent / 'data'
+TEST_DATA_DIR = Path(__file__).parent / "data"
 
 
 def get_process_listening_port(proc):
-    psutil_proc = psutil.Process(proc.pid)
-    while not any(conn.status == 'LISTEN' for conn in psutil_proc.connections()):
-        time.sleep(0.01)
+    conn = None
+    current_process = psutil.Process(proc.pid)
+    children = current_process.children(recursive=True)
+    for child in children:
+        if child.connections() != []:
+            while not any(conn.status == "LISTEN" for conn in child.connections()):
+                print(f"waiting {child.connections()}")
+                time.sleep(0.01)
 
-    conn = next(filter(lambda conn: conn.status == 'LISTEN', psutil_proc.connections()))
+            conn = next(
+                filter(lambda conn: conn.status == "LISTEN", child.connections())
+            )
     print(conn.laddr.port)
     return conn.laddr.port
 
@@ -27,23 +35,34 @@ def get_eel_server(example_py, start_html):
     test = None
 
     try:
-        with tempfile.NamedTemporaryFile(mode='w', dir=os.path.dirname(example_py), delete=False) as test:
+        with tempfile.NamedTemporaryFile(
+            mode="w", dir=os.path.dirname(example_py), delete=False
+        ) as test:
             # We want to run the examples unmodified to keep the test as realistic as possible, but all of the examples
             # want to launch browsers, which won't be supported in CI. The below script will configure eel to open on
             # a random port and not open a browser, before importing the Python example file - which will then
             # do the rest of the set up and start the eel server. This is definitely hacky, and means we can't
             # test mode/port settings for examples ... but this is OK for now.
-            test.write(f"""
+            test.write(
+                f"""
+import os
+print("This process has the PID", os.getpid())
 import eel
 
 eel._start_args['mode'] = None
 eel._start_args['port'] = 0
 
 import {os.path.splitext(os.path.basename(example_py))[0]}
-""")
-
-        proc = subprocess.Popen(['python', test.name], cwd=os.path.dirname(example_py))
+"""
+            )
+        proc = subprocess.Popen(
+            [sys.executable, test.name], shell=True, cwd=os.path.dirname(example_py)
+        )
+        print(f"I think it should be this one {proc.pid}")
         time.sleep(1)
+        # test_port = get_process_listening_port(proc)
+        # print(test_port)
+        # eel_port = test_port
         eel_port = get_process_listening_port(proc)
 
         yield f"http://localhost:{eel_port}/{start_html}"
@@ -59,10 +78,10 @@ import {os.path.splitext(os.path.basename(example_py))[0]}
 
 
 def get_console_logs(driver, minimum_logs=0):
-    console_logs = driver.get_log('browser')
+    console_logs = driver.get_log("browser")
 
     while len(console_logs) < minimum_logs:
-        console_logs += driver.get_log('browser')
+        console_logs += driver.get_log("browser")
         time.sleep(0.1)
 
     return console_logs
