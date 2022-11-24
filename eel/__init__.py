10a53d966dc7b310d6f4fe06883c25ffd4e25b27
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2022-11-24 09:48:33 +0900

    Use _MEIPASS only for pyinstaller frozen apps

--- eel/__init__.py
+++ eel/__init__.py
@@ -312,7 +312,7 @@ def _process_message(message, ws):
 
 
 def _get_real_path(path):
-    if getattr(sys, 'frozen', False):
+    if getattr(sys, 'frozen', False) and hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, path)
     else:
         return os.path.abspath(path)
