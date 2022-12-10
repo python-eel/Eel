ece4f321683bd6f8316cc17a3ab18df956a1f2fd
doug-benn <fiery.coder@gmail.com>
2022-12-10 09:27:30 +0000

    Fixed Process ID issue on Windows Python 3.6 subprocess bug hack

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,11 @@
 import contextlib
 import os
+import sys
+import platform
 import subprocess
+
+# Hack for python 3.6
+subprocess._cleanup = lambda: None
 import tempfile
 import time
 from pathlib import Path
@@ -8,15 +13,33 @@ from pathlib import Path
 import psutil
 
 # Path to the test data folder.
-TEST_DATA_DIR = Path(__file__).parent / 'data'
+TEST_DATA_DIR = Path(__file__).parent / "data"
 
 
 def get_process_listening_port(proc):
-    psutil_proc = psutil.Process(proc.pid)
-    while not any(conn.status == 'LISTEN' for conn in psutil_proc.connections()):
-        time.sleep(0.01)
+    # psutil_proc = psutil.Process(proc.pid)
+    # while not any(conn.status == "LISTEN" for conn in psutil_proc.connections()):
+    #     time.sleep(0.01)
 
-    conn = next(filter(lambda conn: conn.status == 'LISTEN', psutil_proc.connections()))
+    # conn = next(filter(lambda conn: conn.status == "LISTEN", psutil_proc.connections()))
+    conn = None
+    if platform.system() == "Windows":
+        current_process = psutil.Process(proc.pid)
+        children = current_process.children(recursive=True)
+        if children == []:
+            children = [current_process]
+        for child in children:
+            if child.connections() != []:
+                while not any(conn.status == "LISTEN" for conn in child.connections()):
+                    time.sleep(0.01)
+
+                conn = next(filter(lambda conn: conn.status == "LISTEN", child.connections()))
+    else:
+        psutil_proc = psutil.Process(proc.pid)
+        while not any(conn.status == "LISTEN" for conn in psutil_proc.connections()):
+            time.sleep(0.01)
+
+        conn = next(filter(lambda conn: conn.status == "LISTEN", psutil_proc.connections()))
     return conn.laddr.port
 
 
@@ -26,22 +49,30 @@ def get_eel_server(example_py, start_html):
     test = None
 
     try:
-        with tempfile.NamedTemporaryFile(mode='w', dir=os.path.dirname(example_py), delete=False) as test:
+        with tempfile.NamedTemporaryFile(mode="w", dir=os.path.dirname(example_py), delete=False) as test:
             # We want to run the examples unmodified to keep the test as realistic as possible, but all of the examples
             # want to launch browsers, which won't be supported in CI. The below script will configure eel to open on
             # a random port and not open a browser, before importing the Python example file - which will then
             # do the rest of the set up and start the eel server. This is definitely hacky, and means we can't
             # test mode/port settings for examples ... but this is OK for now.
-            test.write(f"""
+            test.write(
+                f"""
 import eel
 
 eel._start_args['mode'] = None
 eel._start_args['port'] = 0
 
 import {os.path.splitext(os.path.basename(example_py))[0]}
-""")
-
-        proc = subprocess.Popen(['python', test.name], cwd=os.path.dirname(example_py))
+"""
+            )
+        if platform.system() == "Windows":
+            proc = subprocess.Popen(
+                [sys.executable, test.name],
+                cwd=os.path.dirname(example_py),
+            )
+        else:
+            proc = subprocess.Popen(["python", test.name], cwd=os.path.dirname(example_py))
+        time.sleep(1)
         eel_port = get_process_listening_port(proc)
 
         yield f"http://localhost:{eel_port}/{start_html}"
@@ -57,10 +88,10 @@ import {os.path.splitext(os.path.basename(example_py))[0]}
 
 
 def get_console_logs(driver, minimum_logs=0):
-    console_logs = driver.get_log('browser')
+    console_logs = driver.get_log("browser")
 
     while len(console_logs) < minimum_logs:
-        console_logs += driver.get_log('browser')
+        console_logs += driver.get_log("browser")
         time.sleep(0.1)
 
     return console_logs
