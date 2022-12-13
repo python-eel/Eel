a10b10502eb6a6a1e94ef76ace034fb8ed011d13
doug-benn <fiery.coder@gmail.com>
2022-12-13 15:11:48 +0000

    Removed "Fix" for Mac&Linux

--- tests/utils.py
+++ tests/utils.py
@@ -32,7 +32,7 @@ def get_process_listening_port(proc):
             conn = next(filter(lambda conn: conn.status == "LISTEN", child.connections()))
     else:
         psutil_proc = psutil.Process(proc.pid)
-        while psutil_proc.connections() == [] and not any(conn is None and conn.status == "LISTEN" for conn in psutil_proc.connections()):
+        while not any(conn is None and conn.status == "LISTEN" for conn in psutil_proc.connections()):
             time.sleep(0.01)
 
         conn = next(filter(lambda conn: conn.status == "LISTEN", psutil_proc.connections()))
