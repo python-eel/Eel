91dd20cc3456e09bf0dfe9b869a2f5ae8ebee4e8
Code Hangen <111701513+codehangen@users.noreply.github.com>
2022-12-16 12:23:52 -0300

    Update main.py

--- examples/10 - charjs/main.py
+++ examples/10 - charjs/main.py
@@ -1,6 +1,18 @@
-@eel.expose	
-def dashboard():
+import eel
 
-	result = [300, 50, 100]
+# Set web files folder
+eel.init('web')
 
-	return (result)
+@eel.expose                         # Expose this function to Javascript
+def dashboard(x):
+    result = [300, 50, 100]
+    return (result)
+    
+@eel.expose                         # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!')   # Call a Javascript function
+
+eel.start('chart.html', size=(300, 200))  # Start
