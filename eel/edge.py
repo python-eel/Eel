463f81d90c4f70b8b79377e54ce58b7aaba8fe05
doug-benn <fiery.coder@gmail.com>
2023-02-13 22:25:59 +0000

    Edge pyinstaller "stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE," issue fix

--- eel/edge.py
+++ eel/edge.py
@@ -2,31 +2,38 @@ import os
 import subprocess as sps
 import sys
 
-name = 'Edge'
+name = "Edge"
+
 
 def run(path, options, start_urls):
-    if path != 'edge_legacy':
-        if options['app_mode']:
+    if path != "edge_legacy":
+        if options["app_mode"]:
             for url in start_urls:
-                sps.Popen([path, '--app=%s' % url] + options['cmdline_args'], 
-                        stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE)
+                sps.Popen([path, "--app=%s" % url] + options["cmdline_args"], stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE)
         else:
-            args = options['cmdline_args'] + start_urls
-            sps.Popen([path, '--new-window'] + args,
-                    stdout=sps.PIPE, stderr=sys.stderr, stdin=sps.PIPE)
+            args = options["cmdline_args"] + start_urls
+            sps.Popen(
+                [path, "--new-window"] + args,
+                stdout=sps.PIPE,
+                stderr=sps.PIPE,
+                stdin=sps.PIPE,
+            )
     else:
-        cmd = 'start microsoft-edge:{}'.format(start_urls[0])
-        sps.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, stdin=sps.PIPE, shell=True)
+        cmd = "start microsoft-edge:{}".format(start_urls[0])
+        sps.Popen(cmd, stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE, shell=True)
+
 
 def find_path():
-    if sys.platform in ['win32', 'win64']:
+    if sys.platform in ["win32", "win64"]:
         return _find_edge_win()
     else:
         return None
 
+
 def _find_edge_win():
     import winreg as reg
-    reg_path = r'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe'
+
+    reg_path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe"
 
     for install_type in reg.HKEY_CURRENT_USER, reg.HKEY_LOCAL_MACHINE:
         try:
@@ -36,7 +43,7 @@ def _find_edge_win():
             if not os.path.isfile(edge_path):
                 continue
         except WindowsError:
-            edge_path = 'edge_legacy'
+            edge_path = "edge_legacy"
         else:
             break
 
