1fb06d72ce22408c8f769a4afe718b3394729f47
doug-benn <fiery.coder@gmail.com>
2023-02-14 20:16:39 +0000

    New Types added

--- eel/edge.py
+++ eel/edge.py
@@ -3,28 +3,25 @@ import subprocess as sps
 import sys
 from typing import List
 
-name = "Edge"
+from eel.types import OptionsDictT
+
+name: str = "Edge"
 
 
-def run(path, options, start_urls):
+def run(path: str, options: OptionsDictT, start_urls: List[str]) -> None:
     if path != "edge_legacy":
         if options["app_mode"]:
             for url in start_urls:
                 sps.Popen([path, "--app=%s" % url] + options["cmdline_args"], stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE)
         else:
             args = options["cmdline_args"] + start_urls
-            sps.Popen(
-                [path, "--new-window"] + args,
-                stdout=sps.PIPE,
-                stderr=sps.PIPE,
-                stdin=sps.PIPE,
-            )
+            sps.Popen([path, "--new-window"] + args, stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE)
     else:
         cmd = "start microsoft-edge:{}".format(start_urls[0])
         sps.Popen(cmd, stdout=sps.PIPE, stderr=sps.PIPE, stdin=sps.PIPE, shell=True)
 
 
-def find_path():
+def find_path() -> bool:
     if sys.platform in ["win32", "win64"]:
         return _find_edge_win()
     else:
