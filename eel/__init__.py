97730065532be36687e52d4a93f3d54150a14b8a
KaminariKatt <88882746+kaminari-katt@users.noreply.github.com>
2024-01-15 20:05:23 +0530

    fix incorrectly imported bottle websocket package with respect to Python version 3.12
    Python 3.12.1 seems to have a problem with importing bottle.ext.websocket. Replacing it with bottle_websocket fixes this.

--- eel/__init__.py
+++ eel/__init__.py
@@ -13,7 +13,7 @@ from gevent.threading import Timer
 import gevent as gvt
 import json as jsn
 import bottle as btl
-import bottle.ext.websocket as wbs
+import bottle_websocket as wbs
 import re as rgx
 import os
 import eel.browsers as brw
